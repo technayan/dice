9081e20203da8f48785b61db2a96b10b2fe84d99
dev_nayan <md.nayan8494@gmail.com>
2022-02-06 16:55:09 +0600

    image link updated

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let dices = ['../img/1.png','../img/2.png','../img/3.png','../img/4.png','../img/5.png','../img/6.png'];
+let dices = ['img/1.png','img/2.png','img/3.png','img/4.png','img/5.png','img/6.png'];
 
 let first = dices[0];
 
